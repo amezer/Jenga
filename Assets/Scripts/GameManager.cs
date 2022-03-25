1158a57c23317f7373d731a0b444cdf7459db960
amezer <jocelyn.her.ho@gmail.com>
2022-03-25 12:17:21 +0800

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class GameManager : MonoBehaviour
+{
+    public Slider volumeSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        AudioListener.volume = volumeSlider.value;
+    }
+}
